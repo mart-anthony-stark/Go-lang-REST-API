38eb04c1f938d1650019f3f3f3ab5716addbc3e0
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-10 21:55:30 +0800

    Add models

--- main.go
+++ main.go
@@ -4,7 +4,39 @@ import(
 	"encoding/json"
 	"log"
 	"math/rand"
-	"net/htpp"
+	"net/http"
 	"strconv"
 	"github.com/gorilla/mux"	
 )
+
+// Book struct model
+type Book struct{
+	ID		string `json:"id"`
+	Isbn	string `json:"isbn"`
+	Title	string `json:"title"`
+	Author	*Author `json:"author"`
+}
+// Author struct
+type Author struct{
+	Firstname string `json:"firstname"`
+	Lastname string `json:"lastname"`
+}
+
+// Initial book
+var books []Book
+
+// Get All books
+func getBooks(w http.ResponseWriter, r *http.Request){
+
+}
+
+func main(){
+	// Init router
+	r := mux.NewRouter()
+
+	// Route handlers enpoint
+	r.HandleFuc("/books", getBooks).Methods("GET")
+
+	// Start server
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
